ba924dc4a77df5f6a36d4cfb493672df41859e8d
cbowke <chris@valorvdc.com>
2025-04-05 18:41:15 -0400

    updated the code for feet and inches

--- ValorVDC_BIMTools/Commands/SpecifyLength/SpecifyLengthWindow.xaml.cs
+++ ValorVDC_BIMTools/Commands/SpecifyLength/SpecifyLengthWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace ValorVDC_BIMTools.Commands.SpecifyLength;
 
@@ -8,20 +9,48 @@ public partial class SpecifyLengthWindow : Window
     public SpecifyLengthWindow(double currentLength)
     {
         InitializeComponent();
-        InputLength.Text = currentLength.ToString("F2");
+        InputLengthFeet.Text = currentLength.ToString();
+    }
+    
+
+    private void SubmitButton_Click(object sender, RoutedEventArgs e)
+    {
+        double feet = 0;
+        if (!string.IsNullOrWhiteSpace(InputLengthFeet.Text))
+        {
+            if (!double.TryParse(InputLengthFeet.Text, out feet) || feet < 0)
+            {
+                MessageBox.Show("Please enter a valid non-negative number for feet.");
+                return;
+            }
+        }
+
+        double inches = 0;
+        if (!string.IsNullOrWhiteSpace((InputLengthInches.Text)))
+        {
+            if (!double.TryParse(InputLengthInches.Text, out inches) || inches < 0)
+            {
+                MessageBox.Show("Please enter a valid non-negative number for inches.");
+                return;
+            }
+        }
+
+        double totalLengthInFeet = feet + (inches / 12);
+        SpecifiedLength = totalLengthInFeet;
+
+        this.DialogResult = true;
+        this.Close();
     }
 
-    private void OKButton_Click(object sender, RoutedEventArgs e)
+    private void InputLengthFeet_TextChanged(object sender, TextChangedEventArgs e)
     {
-        if (double.TryParse(InputLength.Text, out double specificedLength) && specificedLength > 0)
-        {
-            SpecifiedLength = specificedLength;
-            DialogResult = true;
-        }
-        else
-        {
-            MessageBox.Show("Please enter a valid positive number.", "Invalid Input", MessageBoxButton.OK,
-                MessageBoxImage.Warning);
-        }
+        if (!string.IsNullOrWhiteSpace(InputLengthFeet.Text))
+            InputLengthInches.Text = string.Empty;
+    }
+
+    private void InputLengthInches_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace(InputLengthInches.Text))
+            InputLengthFeet.Text = string.Empty;
     }
 }
