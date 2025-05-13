ed83ff6537d3539a6f8f7d013007c3cdbdf7a48a
cbowke <chris@valorvdc.com>
2025-05-13 08:15:27 -0400

    Began making Wall Sleves a Wpf for more selection Options

---
+++ ValorVDC_BIMTools/Commands/WallSleeve/Views/WallSleevesView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace ValorVDC_BIMTools.Commands.Views;
+
+public partial class WallSleevesView : Window
+{
+    public WallSleevesView(WallSleevesView viewModel)
+    {
+        InitializeComponent();
+        DataContext = viewModel;
+
+        viewModel.RequestClose += DialogResult = viewModel.DialogResult;
+    }
+}
